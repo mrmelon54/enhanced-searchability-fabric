538492185ba342d77e3f879cc168ae508ee19370
MrMelon <github@onpointcoding.net>
2022-01-16 00:18:39 +0000

    Bump to v2.0.1 and fixed a crash when using both Enchanted Searchability and Skin Swapper

--- src/main/java/net/onpointcoding/enhancedsearchability/mixin/MixinMultiplayerScreen.java
+++ src/main/java/net/onpointcoding/enhancedsearchability/mixin/MixinMultiplayerScreen.java
@@ -1,7 +1,6 @@
 package net.onpointcoding.enhancedsearchability.mixin;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerServerListWidget;
@@ -10,19 +9,24 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.onpointcoding.enhancedsearchability.duck.ListWidgetDuckProvider;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MultiplayerScreen.class)
+import java.util.List;
+
+@Mixin(value = MultiplayerScreen.class)
 public class MixinMultiplayerScreen extends Screen {
     @Shadow
     protected MultiplayerServerListWidget serverListWidget;
     @Shadow
     private boolean initialized;
+    @Shadow
+    @Nullable
+    private List<Text> tooltipText;
     private TextFieldWidget serverSearchBox;
 
     protected MixinMultiplayerScreen(Text title) {
@@ -39,9 +43,15 @@ public class MixinMultiplayerScreen extends Screen {
             setupOriginalServerListOffset(this.serverListWidget);
     }
 
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/multiplayer/MultiplayerScreen;drawCenteredText(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;III)V"))
-    private void redirected_drawCenteredText(MatrixStack matrixStack, TextRenderer textRenderer, Text text, int centerX, int y, int color) {
-        drawCenteredText(matrixStack, this.textRenderer, this.title, this.width / 2, 8, 16777215);
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/multiplayer/MultiplayerScreen;drawCenteredText(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;III)V", shift = At.Shift.BEFORE), cancellable = true)
+    private void injected_render(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        drawCenteredText(matrices, this.textRenderer, this.title, this.width / 2, 8, 16777215);
+        super.render(matrices, mouseX, mouseY, delta);
+        if (this.tooltipText != null) {
+            this.renderTooltip(matrices, this.tooltipText, mouseX, mouseY);
+        }
+        this.serverSearchBox.render(matrices, mouseX, mouseY, delta);
+        ci.cancel();
     }
 
     void setupOriginalServerListOffset(MultiplayerServerListWidget multiplayerServerListWidget) {
@@ -57,10 +67,5 @@ public class MixinMultiplayerScreen extends Screen {
         });
         this.addSelectableChild(textFieldWidget);
         return textFieldWidget;
-    }
-
-    @Inject(method = "render", at = @At("TAIL"))
-    private void injected_render(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        this.serverSearchBox.render(matrices, mouseX, mouseY, delta);
     }
 }
